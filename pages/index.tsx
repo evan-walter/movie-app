6a13f2891d7577c9397ab1c51f7b4d6f20549892
evan-walter <ewpw5678@gmail.com>
2023-07-21 11:54:21 -0700

    WIP: Implement more login functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
+
+export async function getServerSideProps(context: NextPageContext) {
+	const session = await getSession(context);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/auth',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
+}
+
 export default function Home() {
 	return (
 		<main>
 			<h1 className='underline'>Movie App</h1>
+			<button onClick={() => signOut()}>Logout</button>
 		</main>
 	);
 }
