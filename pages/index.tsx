ecde5a2d60dff736fd389688038e2992ae77ded8
evan-walter <ewpw5678@gmail.com>
2023-08-07 00:15:33 -0400

    Format with prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,31 +4,31 @@ import useCurrentUser from '@/hooks/useCurrentUser';
 import Navbar from '@/components/Navbar';
 
 export async function getServerSideProps(context: NextPageContext) {
-	const session = await getSession(context);
+  const session = await getSession(context);
 
-	if (!session) {
-		return {
-			redirect: {
-				destination: '/auth',
-				permanent: false,
-			},
-		};
-	}
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
 
-	return {
-		props: {},
-	};
+  return {
+    props: {},
+  };
 }
 
 export default function Home() {
-	const { data: user } = useCurrentUser();
+  const { data: user } = useCurrentUser();
 
-	return (
-        <>
-            <div className='mt-16 h-screen'>
-                <Navbar />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className='mt-16 h-screen'>
+        <Navbar />
+      </div>
+    </>
+  );
 }
 
