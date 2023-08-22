1e96ff89defa536f12d40bac08500b0c14b662ec
evan-walter <ewpw5678@gmail.com>
2023-08-22 15:48:39 -0700

    Finish random endpoint

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -13,6 +13,16 @@ export default async function handler(
 
   try {
     await serverAuth(req);
+
+    const movieCount = await prismadb.movie.count();
+    const randomIndex = Math.floor(Math.random() * movieCount);
+
+    const randomMovies = await prismadb.movie.findMany({
+      take: 1,
+      skip: randomIndex,
+    });
+
+    return res.status(200).json(randomMovies[0]);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
