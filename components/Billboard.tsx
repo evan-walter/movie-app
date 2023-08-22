2a8f36a8cfa71da3642132eeea920a8c629eb1d2
evan-walter <ewpw5678@gmail.com>
2023-08-22 16:17:47 -0700

    Create Billboard front end

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,3 +1,25 @@
+import useBillboard from '@/hooks/useBillboard';
+
 export default function Billboard() {
-  return <div>Billboard</div>;
+  const { data } = useBillboard();
+
+  return (
+    <div>
+      <h1>Billboard</h1>
+      <video
+        autoPlay
+        muted
+        loop
+        poster={data?.thumbnailUrl}
+        src={data?.videoUrl}
+      ></video>
+      <div>
+        <p>{data?.title}</p>
+        <p>{data?.description}</p>
+      </div>
+      <div>
+        <button>More Info</button>
+      </div>
+    </div>
+  );
 }
